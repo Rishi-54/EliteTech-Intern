db6de8d869b0f3965fb3eebd4790e8269d15c711
Rishi-54 <rishiramavath54@gmail.com>
2025-01-03 12:33:53 +0530

    Update acronym.py

--- Elite/acronym.py
+++ Elite/acronym.py
@@ -3,6 +3,6 @@ def create_acronym(word):
     acronym = ''.join(word[0].upper() for word in words)
     return acronym
 
-# Example usage
+
 word = input("Enter word: ")
 print(create_acronym(word))  
